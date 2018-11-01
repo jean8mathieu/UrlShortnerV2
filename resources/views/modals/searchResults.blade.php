9b1d83a89e6d42a778fe151345968e08b61e2a3b
Jean-Mathieu <jean-mathieu@jmdev.ca>
2018-10-31 20:47:29 -0400

    Missing file

---
+++ resources/views/modals/searchResults.blade.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="searchResultModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <h4 class="text-center">Your Search Result</h4>
+            </div>
+            <div class="modal-body" id="searchResultTable">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
